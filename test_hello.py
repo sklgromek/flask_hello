101977ba454bf3f485f72720f3ade15f100a8ea2
Tomasz Gromowski <t.gromowski@norwida.net>
2021-03-24 14:00:00 +0100

    initial

---
+++ test_hello.py
@@ -0,0 +1,8 @@
+# test_app.py
+from hello import app
+
+def test_hello():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
